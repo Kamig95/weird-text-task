e2c387c6d844dae478c974a37c65dfc3927560b7
kamil.grabowski <kamil.grabowski@jmr.pl>
2022-04-18 20:50:51 +0200

    fixed app

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from weird_text import encode, decode
+
+app = Flask(__name__)
+
+
+@app.route("/v1/encode")
+def encode_endpoint():
+    return encode(request.args.get("text"))
+
+
+@app.route("/v1/decode")
+def decode_endpoint():
+    text = request.args.get("text")
+    return decode(text)
+
