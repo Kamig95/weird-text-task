dd7f577e662b9729336310c0ac8155e2a32a8000
kamil.grabowski <kamil.grabowski@jmr.pl>
2022-04-18 20:55:18 +0200

    welcome site

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 from flask import Flask, request
-from weird_text import encode, decode
+# from weird_text import encode, decode
 
 app = Flask(__name__)
 
 
-@app.route("/v1/encode")
-def encode_endpoint():
-    return encode(request.args.get("text"))
+# @app.route("/v1/encode")
+# def encode_endpoint():
+#     return encode(request.args.get("text"))
+#
+#
+# @app.route("/v1/decode")
+# def decode_endpoint():
+#     text = request.args.get("text")
+#     return decode(text)
 
-
-@app.route("/v1/decode")
-def decode_endpoint():
-    text = request.args.get("text")
-    return decode(text)
-
+@app.route("/")
+def home_view():
+        return "<h1>Welcome to my site</h1>"
